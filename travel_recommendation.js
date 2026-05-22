02a62da34f4dc992ec81f081dad3ef2fe72d0107
venancio96 <venancio.cototico@gmail.com>
2026-05-22 05:04:59 -0400

    still adding

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,27 +1,32 @@
 const searchBtn = document.getElementById("searchBtn");
 function Search()
 {
-    console.log("hello");
     const input = document.getElementById("searchInput").value.toLowerCase();
     //const resultDiv = "";
     //resultDiv.innerHTML="";
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-       // const test = countires.cities[1].name;
-      // console.log('test');
-        const answer = data.countries.toLowerCase() === input;
-        if(answer)
-        {
-            console.log(answer.cities.name);
-        //const  name = country.cities.name.join(',');
-        //resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-        }
-        else
-        {
-            console.log("no citiy found");
-        }
-    
+    if(input === "countries")
+    {
+        console.log("countries");
+    }
+    else if(input === "beach")
+    {
+        console.log("beach")
+    }
+    else if(input === "temple")
+    {
+        console.log("temple");
+    }
+    else
+    {
+        console.log("nothingfound");
+    }
     })
+    .catch(error =>{
+    console.error('Error:', error);
+			console.log("error");
+    });
 }
 searchBtn.addEventListener('click', Search);
